0840c61939516df9332f8c28d95f93f3e4d64f7a
sevvalsaglam <sevvallsaglam@gmail.com>
2025-04-28 12:51:40 +0300

    commit

---
+++ src/EmojiBackground.tsx
@@ -0,0 +1,14 @@
+import './EmojiBackground.css'; // Emoji'leri kontrol edeceğimiz CSS dosyası
+
+const EmojiBackground = () => {
+  return (
+    <div className="emoji-background">
+      <div className="emoji">🍿</div>
+      <div className="emoji">🎬</div>
+      <div className="emoji">🎞️</div>
+      <div className="emoji">🎥</div>
+    </div>
+  );
+};
+
+export default EmojiBackground;
